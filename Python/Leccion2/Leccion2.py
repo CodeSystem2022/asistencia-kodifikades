672d480f47b8b87e1f10ebf25555780545946804
Juan Cruz Reche <juancruzreche17@gmail.com>
2022-06-02 00:48:50 -0300

    Ejercicio 1

--- Python/Leccion2/Leccion2.py
+++ Python/Leccion2/Leccion2.py
@@ -1,4 +1,5 @@
 #En esta calse veremos la sentencia if else condicion = 1.0
+"""
 condicion = 10
 if condicion==True:
     print("Condicion verdadera")
@@ -6,3 +7,17 @@ elif condicion==False:
     print("Condicion falsa")
 else:
     print("condicion sin especificar")
+"""
+#Conversion de numero a texto
+num = int(input("Ingrese un numero en el rango del 1 al 3"))
+numTexto = ""
+if num == 1:
+    numTexto = "Numero uno"
+elif num == 2:
+    numTexto = "Numero dos"
+elif num == 3:
+    numTexto = "Numero tres"
+else:
+    print("Has ingresado un numero fuera del rango")
+
+print(f"El numero ingresado es {numTexto}")
