84616e86bd0ab7db98b112479a5040759dd66407
Juan Cruz Reche <juancruzreche17@gmail.com>
2022-06-02 00:12:14 -0300

    Primer commit Leccion_2

---
+++ Python/Leccion2/Leccion2.py
@@ -0,0 +1,8 @@
+#En esta calse veremos la sentencia if else condicion = 1.0
+condicion = 10
+if condicion==True:
+    print("Condicion verdadera")
+elif condicion==False:
+    print("Condicion falsa")
+else:
+    print("condicion sin especificar")
