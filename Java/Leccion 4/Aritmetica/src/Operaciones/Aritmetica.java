fa83f620dfb1125b1447d5cd6e33c3b52cd4423e
Juan Cruz Reche <juancruzreche17@gmail.com>
2022-10-16 20:09:59 -0300

    Java clase 5 clases y objetos

--- Java/Leccion 4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion 4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -10,6 +10,16 @@ public class Aritmetica {
     }
     public int sumarConRetorno(){
         //int resultado = a + b;
-        return a + b;
+        return this.a + this.b;
     }
+    
+    public int sumarConArgumentos(int a, int b){ //No modifica el valor de los atributos
+        this.a = a; //El argumeto "a" se asigna al atributo this.a
+        this.b = b;
+        //return a + b;
+        return this.sumarConRetorno(); //Se llama a un metodo desde otro metodo (Esto se debe hacer solo con metodos que esten dentro de una misma clase)
+        
+    }
+    
+
 }
--- Java/Leccion 4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/Leccion 4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -3,13 +3,18 @@ package Operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
-    Aritmetica aritmetica1 = new Aritmetica();
+    Aritmetica aritmetica1 = new Aritmetica();  // Se llama al constructor, es decir, se reserva un espacio en la memoria
     aritmetica1.a = 3;
     aritmetica1.b = 7;
     aritmetica1.sumarNumeros();
     
     int resultado = aritmetica1.sumarConRetorno();
         System.out.println("resultado = " + resultado);
+    
+    resultado = aritmetica1.sumarConArgumentos(12, 26);
+        System.out.println("Resultado usando argumentos: "+resultado);
+        
+    
     }
     
    
