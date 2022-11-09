35ebee5bbf29bb7e0f36c14ab1236dcf27afcb6f
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2022-11-08 21:25:11 -0300

    Delete Vehiculos.py

--- Vehiculos.py
+++
@@ -1,7 +0,0 @@
-class Vehiculo:
-    def __init__(self, color, ruedas):
-        self.color = color
-        self.ruedas = ruedas
-
-    def __str__(self):
-        return 'Color: ' + self.color + ', Ruedas: ' + str(self.ruedas)
