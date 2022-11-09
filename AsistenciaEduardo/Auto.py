7949a774c7a9fc0753d4715fc25532397f4fde0b
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2022-11-08 21:26:21 -0300

    Asistencia Eduardo Bru
    Asistencia Eduardo Bru

---
+++ AsistenciaEduardo/Auto.py
@@ -0,0 +1,9 @@
+from Vehiculos import *
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__() + ', Velocidad(km/hr): ' + str(self.velocidad)
+
