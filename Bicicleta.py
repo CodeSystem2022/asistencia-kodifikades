78e2058407a62a77717f17e2a4de209740bf70f6
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2022-11-08 21:24:52 -0300

    Delete Bicicleta.py

--- Bicicleta.py
+++
@@ -1,8 +0,0 @@
-from Vehiculos import *
-class Bicicleta(Vehiculo):
-    def __init__(self, color, ruedas, tipo):
-        super().__init__(color, ruedas)
-        self.tipo = tipo
-
-    def __str__(self):
-        return super().__str__() + ', Tipo: ' + self.tipo
